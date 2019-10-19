a2f50693e972005684375cca9fb53c0df46091d5
ly9999y <478125686@qq.com>
2019-10-19 23:52:18 +0800

    feat:使用@ConfigurationProperties实现优雅的注入

--- springboot-demo/src/main/java/com/leyou/JdbcConfig.java
+++ springboot-demo/src/main/java/com/leyou/JdbcConfig.java
@@ -1,7 +1,7 @@
 package com.leyou;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -11,22 +11,10 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("classpath:jdbc.properties")
 public class JdbcConfig {
-	@Value("${jdbc.url}")
-	String url;
-	@Value("${jdbc.driverClassName}")
-	String driverClassName;
-	@Value("${jdbc.username}")
-	String username;
-	@Value("${jdbc.password}")
-	String password;
-
 	@Bean
-	public DataSource dataSource(){
-		DruidDataSource druidDataSource = new DruidDataSource();
-		druidDataSource.setUrl(url);
-		druidDataSource.setDriverClassName(driverClassName);
-		druidDataSource.setUsername(username);
-		druidDataSource.setPassword(password);
-		return druidDataSource;
+	@ConfigurationProperties(prefix = "jdbc")
+	public DataSource dataSource() {
+		DruidDataSource dataSource = new DruidDataSource();
+		return dataSource;
 	}
 }
